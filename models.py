0c5e13c0e7d479d93f63e97bb96372d0e719a39c
Rukaiya Khan <133051015+Khanrukku@users.noreply.github.com>
2024-07-10 10:29:26 -0700

    Create models.py

---
+++ models.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    daily_goal = db.Column(db.Float, default=0.0)
+    water_records = db.relationship('WaterRecord', backref='user', lazy=True)
+
+class WaterRecord(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Float, nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
